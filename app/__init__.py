8ce9c07ef5545ac51fb8c965353e704c1500a3f1
llzz <2270038123@qq.com>
2021-04-02 20:54:54 +0800

    项目拆分

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-
+import os
 from tornado.web import Application
 
 from app.views.cookie_v import CookieHandler
@@ -6,11 +6,19 @@ from app.views.index_v import IndexHandler
 from app.views.order_v import OrderHandler
 from app.views.search_v import SearchHandler
 
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+print(BASE_DIR)
+settings = {
+    'debug': True,
+    'template_path': os.path.join(BASE_DIR, 'templates')
+}
+
 
 def make_app(host='localhost'):
-    return Application([
+    return Application(handlers=[
         ('/', IndexHandler),
         ('/search', SearchHandler),
         ('/cookie', CookieHandler),
         (r'/order/(\d+)/(\d+)', OrderHandler)
-    ], default_host=host)
+    ], default_host=host,
+         **settings)
--- app/views/index_v.py
+++ app/views/index_v.py
@@ -3,6 +3,9 @@ from tornado.web import RequestHandler
 
 class IndexHandler(RequestHandler):
     def get(self):
+        self.render('index.html',
+                    msg='Hi,Template')
+    def post(self):
         # 向客户端响应数据
         name = self.get_query_argument('name')
         city = self.get_query_argument('city')
@@ -10,7 +13,7 @@ class IndexHandler(RequestHandler):
         self.write('<h3>Hello, Tornado_get %s %s</h3>' % (name, city))
 
 
-    def post(self):
+    def put(self):
         name = self.get_argument('name')
         city = self.get_argument('city')
         self.write('<h3>%s %s</h3>' % (name, city))
