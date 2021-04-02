b7ea83e137eed47f4c80092ea57cb491e21de2f1
llzz <2270038123@qq.com>
2021-04-02 21:04:01 +0800

    项目拆分

--- app/__init__.py
+++ app/__init__.py
@@ -6,11 +6,12 @@ from app.views.index_v import IndexHandler
 from app.views.order_v import OrderHandler
 from app.views.search_v import SearchHandler
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-print(BASE_DIR)
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
 settings = {
     'debug': True,
-    'template_path': os.path.join(BASE_DIR, 'templates')
+    'template_path': os.path.join(BASE_DIR, 'templates'),
+    'static_path': os.path.join(BASE_DIR, 'static')
 }
 
 
